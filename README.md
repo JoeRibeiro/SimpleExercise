390a6e10f754a3509d1e904bdaa98055e574a0b4
Joseph Ribeiro (Cefas) <joseph.ribeiro@cefas.gov.uk>
2023-12-20 17:21:50 +0000

    Change to convex hull approach for measuring distance (removes rotatedrect). Sorts result by numeric (rather than string interpretation which was wrong). Runs it all as functions called from command line argument and is more verbose in case there are issues running the code. Updated readme to reflect this and superfluous code removed.

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Before running the python script planktonsize.py please create a new python envi
 
 
 After navigating to the directory into which this repository has been unpacked, please run planktonsize.py within this environment, for example in miniforge prompt:
-	cd C:\Users\JR13\OneDrive - CEFAS\My onedrive documents\SimpleExercise
-	conda activate envpy3 & python planktonsize.py
+	
+	cd "C:\Users\yourPC\SimpleExercise"
+	conda activate envpy3 & python planktonsize.py "C:\Users\yourPC\SimpleExercise\data\raw"`
+	
+If you have issues running, first please check you are indeed (1) running from the "SimpleExercise" folder (2) envpy3 enironment has been activated and (3) you are pointing the command towards your data\raw folder.
--- planktonsize.py
+++ planktonsize.py
@@ -1,102 +1,93 @@
 # import the necessary packages
 from scipy.spatial import distance as dist
-from imutils import perspective
-from imutils import contours
 import numpy as np
-import argparse
-import imutils
 import cv2
-from os import walk
+import imutils
+from imutils import contours
+from itertools import combinations
+import sys
+from os import walk, path
 
 def midpoint(ptA, ptB):
-	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-# Set the path to the image folder
-image_folder = r'C:\Users\JR13\OneDrive - CEFAS\My onedrive documents\SimpleExercise\data\raw\\'
+def analyze_plankton(image_folder):
+    # Get the list of filenames in the specified folder
+    try:
+        filenames = next(walk(image_folder), (None, None, []))[2]
+    except StopIteration:
+        print(f"Error: No files found in the specified folder: {image_folder}")
+        sys.exit(1)
 
-# Display a message with the image folder location
-print("Hi! An image window should pop up. Press any key to cycle through images. Please set the image folder location in planktonsize.py. We will be looking in: " + image_folder)
+    # Initialize lists to store plankton lengths and corresponding file names
+    plengths = []
+    fnames = []
 
-# Get the list of filenames in the specified folder
-filenames = next(walk(image_folder), (None, None, []))[2]
+    # Iterate through each image file in the folder
+    for filename in [path.join(image_folder, myname) for myname in filenames]:
+        print(f"Processing image: {filename}")
 
-# Initialize lists to store plankton lengths and corresponding file names
-plengths = []
-fnames = []
+        # Load the image and check if it's loaded successfully
+        try:
+            image = cv2.imread(filename)
+            if image is None:
+                print(f"Error: Unable to load image: {filename}")
+                continue  # Skip to the next image if loading fails
+        except Exception as e:
+            print(f"Error: {e}")
+            continue  # Skip to the next image in case of an exception
 
-# Flag to indicate whether to use brute force method for alternative distance calculation
-brute_force = False
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Iterate through each image file in the folder
-for filename in [image_folder + myname for myname in filenames]:
-    # Load the image and convert it to grayscale
-    image = cv2.imread(filename)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Perform edge detection, followed by dilation and erosion to close gaps
+        edged = cv2.Canny(gray, 30, 100)  # 30 on the Canny picks up fine edges on copepods
+        edged = cv2.dilate(edged, None, iterations=1)
+        edged = cv2.erode(edged, None, iterations=1)
 
-    # Perform edge detection, followed by dilation and erosion to close gaps
-    edged = cv2.Canny(gray, 30, 100)  # 30 on the Canny picks up fine edges on copepods
-    edged = cv2.dilate(edged, None, iterations=1)
-    edged = cv2.erode(edged, None, iterations=1)
+        # Find contours in the edge map
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
 
-    # Find contours in the edge map
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+        # Sort contours from left-to-right
+        (cnts, _) = contours.sort_contours(cnts)
 
-    # Sort contours from left-to-right and initialize the 'pixels per metric' calibration variable
-    (cnts, _) = contours.sort_contours(cnts)
+        # Identify the contour of the plankton (largest contour)
+        if not cnts:
+            print(f"Warning: No contours found in image: {filename}")
+            continue  # Skip to the next image if no contours are found
 
-    # Identify the contour of the plankton (largest contour)
-    c = max(cnts, key=cv2.contourArea)
+        c = max(cnts, key=cv2.contourArea)
 
-    # Compute the rotated bounding box of the contour
-    orig = image.copy()
-    box = cv2.minAreaRect(c)
-    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-    box = np.array(box, dtype="int")
+        # Calculate distance using convex hull of the contour. This finds the two furthest points by comparing all combinations of points
+        objectlength = 0
+        c = cv2.convexHull(c)
+        for acoords, bcoords in combinations(c, 2):
+            # Compute Euclidean distances between points
+            dc = abs(dist.euclidean((acoords[0][0], acoords[0][1]), (bcoords[0][0], bcoords[0][1])))
+            if dc > objectlength:
+                objectlength = dc
 
-    # Order the points in the contour to appear in top-left, top-right, bottom-right, and bottom-left order
-    box = perspective.order_points(box)
+        print(f"Plankton length: {objectlength}")
 
-    # Draw the outline of the rotated bounding box and original points
-    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+        # Save file name and length to a list
+        fnames.append(filename)
+        plengths.append(round(objectlength))
 
-    # Loop over the original points and draw them
-    for (x, y) in box:
-        cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+    if not fnames:
+        print("No valid images found. Exiting.")
+        sys.exit(1)
 
-    # Compute midpoints between different pairs of bounding box points
-    (tl, tr, br, bl) = box
-    (tltrX, tltrY) = midpoint(tl, tr)
-    (blbrX, blbrY) = midpoint(bl, br)
-    (tlblX, tlblY) = midpoint(tl, bl)
-    (trbrX, trbrY) = midpoint(tr, br)
+    result = np.array([fnames, plengths]).T
+    np.savetxt("plankton_lengths.csv", result[result[:, 1].astype(np.int).argsort()], delimiter=",", comments='', fmt='%s')
 
-    # Draw lines between midpoints
-    cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)), (255, 0, 255), 2)
-    cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)), (255, 0, 255), 2)
+if __name__ == "__main__":
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <image_folder_path>")
+        sys.exit(1)
 
-    # Compute Euclidean distances between midpoints
-    dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-    dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+    # Get the image folder path from the command line
+    image_folder_path = sys.argv[1]
 
-    # Calculate an alternative distance using a brute force method if specified
-    alternative_distance = min(dA, dB)
-    if brute_force:
-        for acoords in c:
-            for bcoords in c:
-                dc = abs(dist.euclidean((acoords[0][0], acoords[0][1]), (bcoords[0][0], bcoords[0][1])))
-                if dc > alternative_distance:
-                    alternative_distance = dc
-        print(alternative_distance)
-    fnames.append(filename)
-    plengths.append(round(alternative_distance))
-
-    # Display the output image with length information
-    cv2.imshow("length in pixels is between " + str(int(max(dA, dB))) + " and " + str(int(alternative_distance)) + " pixels", orig)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-result = np.array([fnames,plengths])
-np.savetxt("plankton_lengths.csv", result[result[:,1].argsort()], delimiter=",", header='string', comments='', fmt='%s')
-
+    # Call the function to analyze plankton
+    analyze_plankton(image_folder_path)
